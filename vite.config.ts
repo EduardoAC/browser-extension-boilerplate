a73c2e509fbb3cdf992ca2fb7d82b2d10ac2bb4e
Eduardo <EduardoAC@users.noreply.github.com>
2024-03-03 15:40:05 +0000

    Introduce aliases for project

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./public/manifest.json";
 
+const root = resolve(__dirname, "src");
+const assetsDir = resolve(root, "assets");
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), crx({ manifest })],
+  resolve: {
+    alias: {
+      "@src": root,
+      "@assets": assetsDir,
+    },
+  },
   optimizeDeps: {
     esbuildOptions: {
       sourcemap: "inline",
