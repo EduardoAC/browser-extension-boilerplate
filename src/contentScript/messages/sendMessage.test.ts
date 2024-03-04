7717eb0eb18a72674670b110485c34b80adf023e
Eduardo <EduardoAC@users.noreply.github.com>
2024-03-04 20:07:55 +0000

    Update initial Test

--- src/contentScript/messages/sendMessage.test.ts
+++ src/contentScript/messages/sendMessage.test.ts
@@ -3,21 +3,23 @@
 import { sendMessage } from "./sendMessage"
 
 describe("sendMessage", () => {
-  it("should send the message correctly", () => {
+  it("should send the message correctly", async () => {
     const message: Message = { type: "counter", subType: "get" }
     const response: MessageResponse = { statusCode: 200, data: 2 }
-    const callbackSpy = vi.fn()
 
     console.log(chrome.runtime)
     chrome.runtime.sendMessage.mockImplementation(
-      (_message: any, callback: any): Promise<unknown> => {
-        return callback(response)
+      (_message: any, callback: any): void => {
+        callback(response)
       }
     )
 
-    sendMessage(message)
+    const promiseResult = await sendMessage(message)
 
-    expect(chrome.runtime.sendMessage).toBeCalledWith(message, callbackSpy)
-    expect(callbackSpy).toBeCalledWith(response)
+    expect(promiseResult).toEqual(2)
+    expect(chrome.runtime.sendMessage).toBeCalledWith(
+      message,
+      expect.any(Function)
+    )
   })
 })
