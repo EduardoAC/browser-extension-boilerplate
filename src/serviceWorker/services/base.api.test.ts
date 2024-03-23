ddf016e46243924a16881de6834acb9a7a0455e0
Eduardo <EduardoAC@users.noreply.github.com>
2024-03-23 16:14:43 +0000

    Introduce vite fetch mock

--- src/serviceWorker/services/base.api.test.ts
+++ src/serviceWorker/services/base.api.test.ts
@@ -1,31 +1,25 @@
 import { vi } from "vitest"
+import createFetchMock from "vitest-fetch-mock"
 import { BaseApi } from "./base.api"
 
-const viFn = vi.fn()
-
-// This is the section where we mock `fetch`
-const unmockedFetch = global.fetch
-global.fetch = viFn
-
-function createFetchResponse(data) {
-  return { json: () => new Promise((resolve) => resolve(data)) }
-}
-
+const fetchMocker = createFetchMock(vi)
 const requestUrl = "http://www.url.com"
-
 describe("Base Api", () => {
+  beforeAll(() => {
+    fetchMocker.enableMocks()
+  })
   afterAll(() => {
-    global.fetch = unmockedFetch
+    fetchMocker.disableMocks()
   })
   beforeEach(() => {
-    viFn.mockClear()
-    viFn.mockResolvedValue(createFetchResponse({}))
+    fetchMocker.mockClear()
   })
   describe("Headers customisation", () => {
     it("Headers provided are present in the API request", () => {
+      fetchMocker.mockResponseOnce(JSON.stringify({ data: "12345" }))
       const baseApiInstance = new BaseApi()
       baseApiInstance.get(requestUrl, {}, { test: "header" })
-      expect(viFn).toBeCalledWith(`${requestUrl}/`, {
+      expect(fetchMocker).toBeCalledWith(`${requestUrl}/`, {
         headers: {
           Authorization: "",
           source: "browser-extension",
